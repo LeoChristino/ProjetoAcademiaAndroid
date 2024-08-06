500dae98061dd98671f4ed87e7e7b6117ba01081
LeoChristino <leo.chrissantos28@gmail.com>
2024-08-06 20:28:26 -0300

    criando classe academia

---
+++ app/src/main/java/br/com/etecia/myapp/Academia.java
@@ -0,0 +1,47 @@
+package br.com.etecia.myapp;
+
+public class Academia {
+    private String titulo;
+    private String descricao;
+    private String nota;
+    private int imgAcademia;
+
+    //método construtor
+
+    public Academia() {
+    }
+
+    //método get e set
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+
+    public int getImgAcademia() {
+        return imgAcademia;
+    }
+
+    public void setImgAcademia(int imgAcademia) {
+        this.imgAcademia = imgAcademia;
+    }
+}
